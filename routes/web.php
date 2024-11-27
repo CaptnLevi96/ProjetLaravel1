76a9bf7ca8ce6da1d9770b1082fca410c9b59d34
Levi Loseke <157930732+CaptnLevi96@users.noreply.github.com>
2024-11-27 17:02:30 -0500

    Routes

--- routes/web.php
+++ routes/web.php
@@ -6,26 +6,29 @@ use App\Http\Controllers\NouveautesController;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\PaymentController;
 
-Route::get('/', [LivreController::class, 'index']);
-
-// Routes pour les livres
-Route::get('/livres', [LivreController::class, 'index'])->name('livres.index');
-Route::get('/livres/create', [LivreController::class, 'create'])->name('livres.create');
-Route::post('/livres', [LivreController::class, 'store'])->name('livres.store');
-Route::get('/livres/{id}', [LivreController::class, 'show'])->name('livres.show');
-Route::delete('/livres/{id}', [LivreController::class, 'destroy'])->name('livres.destroy');
-Route::get('/recherche', [LivreController::class, 'search'])->name('livres.search');
-
-// Routes pour le contact et les messages
-Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
-Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
-Route::get('/nouveautes', [NouveautesController::class, 'index'])->name('nouveautes.index');
-Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
-Route::get('/payment/form', [PaymentController::class, 'showPaymentForm'])->name('payment.form');
-Route::post('/payment/pay', [PaymentController::class, 'pay'])->name('payment.pay');
-Route::get('/payment/success', [PaymentController::class, 'success'])->name('payment.success');
-Route::get('/payment/error', [PaymentController::class, 'error'])->name('payment.error');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/livres', [LivreController::class, 'index'])->name('livres.index');
+    Route::get('/livres/create', [LivreController::class, 'create'])->name('livres.create');
+    Route::post('/livres', [LivreController::class, 'store'])->name('livres.store');
+    Route::get('/livres/{id}', [LivreController::class, 'show'])->name('livres.show');
+    Route::delete('/livres/{id}', [LivreController::class, 'destroy'])->name('livres.destroy');
+    Route::get('/recherche', [LivreController::class, 'search'])->name('livres.search');
+
+    Route::get('/contact', [ContactController::class, 'index'])->name('contact.index');
+    Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+    Route::get('/nouveautes', [NouveautesController::class, 'index'])->name('nouveautes.index');
+    Route::get('/messages', [MessageController::class, 'index'])->name('messages.index');
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::get('/payment/form', [PaymentController::class, 'showPaymentForm'])->name('payment.form');
+    Route::post('/payment/pay', [PaymentController::class, 'pay'])->name('payment.pay');
+    Route::get('/payment/success', [PaymentController::class, 'success'])->name('payment.success');
+    Route::get('/payment/error', [PaymentController::class, 'error'])->name('payment.error');
+});
